45b73de9c111dc131c067789d53c2dc100257598
Sakshi Dattatray Ambhure <ambhuresakshi565@gmail.com>
2026-04-23 15:08:05 +0530

    delete footer code

--- my-navbar-app/src/App.js
+++ my-navbar-app/src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import Navbar from './component/Navbar';
 import logo from './logo.svg';
 import './App.css';
-import Footer from './component/Footer';
 // import'./index.css';
 
 function App() {
   return (
    <div>
      <Navbar />
-     <Footer/>
+   
 </div>
   );
 }
