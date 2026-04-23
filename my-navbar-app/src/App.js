441961bc4d3b8d96e4ec65f6744e694247a69a57
Sakshi Dattatray Ambhure <ambhuresakshi565@gmail.com>
2026-04-23 14:45:42 +0530

    Added footer and updated UI

--- my-navbar-app/src/App.js
+++ my-navbar-app/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './component/Navbar';
 import logo from './logo.svg';
 import './App.css';
+import Footer from './component/Footer';
 // import'./index.css';
 
 function App() {
   return (
    <div>
      <Navbar />
+     <Footer/>
 </div>
   );
 }
